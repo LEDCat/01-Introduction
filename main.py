cde4e134d8b6409407d1f9a4b3d2284659d7fd5d
LEDCat <lucaed64@gmail.com>
2019-08-31 22:08:49 -0400

    Completed task.

--- main.py
+++ main.py
@@ -5,4 +5,19 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, my name is Luca!')
+menu = ' '
+while (menu != 'end'):
+    print('\n 1 - Favorite Game\n 2 - Concerns\n 3 - Excitements\n 4 - Stackoverflow\n 5 - Github\n')
+    menu = input('What would you like to know about me? \n\n')
+    menu = menu.strip().lower()
+    if (menu == '1'):
+        print('My favorite games are Borderlands and Pokemon.\n')
+    elif (menu == '2'):
+        print('I may have concerns about keeping up with unfamiliar programs, such as Godot.\n')
+    elif (menu == '3'):
+        print("Despite my worries, I'm still excited aboout possibly learning more about game languages and game development.\n")
+    elif (menu == '4'):
+        print("My stackoverflow.com number is 11981317.\n")
+    elif (menu == '5'):
+        print("My Github URL is https://github.com/LEDCat.\n")
